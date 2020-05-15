1f7334101d441c3f5b31c7b00a6594a953cf5aa7
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-15 23:19:24 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 function sendPushNotification($to = '', $data = array()){
-	$apiKey = '';
+	$apiKey = 'AAAA4QcnW_8:APA91bEdc3LdsGOhcGrDuhalIi2apF-xg35gBZiqDD_L4PJ87iCLAgkioT0HKf7Yd_6BSr4YoikwgGxuWukQw8abcn9N0a1yinyshSmfpwS4G50oQQeG6eswTZ1FYq4-b71qhh7T4sPA';
 	$fields = array( 'to' => $to,	'notfication' => $data );
 	
 	$headers = array( 'Authorization: keys='.$apiKey, 'Content-Type: application/json');
@@ -19,7 +19,7 @@ function sendPushNotification($to = '', $data = array()){
 	$result = curl_exec($ch);
 	return json_decode($result, true);
 }
-$to ="";
+$to =""fOyoviWU-EQ:APA91bFYYOot6p1I8P42pi_zPQrGf8Lmzl9tYH4YEVPF-ECmmFGe_-nKMoeVa2dzX7ikAs_OBeZXK8MoY8YhpZq0AOT0Ne47NoKHRfSz_0Etk9W-7IG0qg39UppREZoIuJbod0-mTtD6"";
 $data = array(
 	'body' => 'New message'
 );
