69b213a321da718fb74d6338904137b292ad1274
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-15 22:00:51 +0800

    Update example.php

--- example.php
+++ example.php
@@ -2,11 +2,10 @@
 <?php
 
  
-function sendPushNotification($device_token = "", $data = array())
- {
-            $API_ACCESS_KEY = 'AAAA4QcnW_8:APA91bEdc3LdsGOhcGrDuhalIi2apF-xg35gBZiqDD_L4PJ87iCLAgkioT0HKf7Yd_6BSr4YoikwgGxuWukQw8abcn9N0a1yinyshSmfpwS4G50oQQeG6eswTZ1FYq4-b71qhh7T4sPA'; // YOUR_FIREBASE_API_KEY
+function sendPushNotification($to = "", $data = array()){
+	$API_ACCESS_KEY = 'AAAA4QcnW_8:APA91bEdc3LdsGOhcGrDuhalIi2apF-xg35gBZiqDD_L4PJ87iCLAgkioT0HKf7Yd_6BSr4YoikwgGxuWukQw8abcn9N0a1yinyshSmfpwS4G50oQQeG6eswTZ1FYq4-b71qhh7T4sPA'; // YOUR_FIREBASE_API_KEY
 			
-			$fields  = array('to' => $device_token, 'notification' => $msg);
+			$fields  = array('to' => $to, 'notification' => $msg);
  
             $headers = array('Authorization: key='.$API_ACCESS_KEY, 'Content-Type: application/json');
  
@@ -27,12 +26,12 @@ function sendPushNotification($device_token = "", $data = array())
             $result = curl_exec($ch);
             curl_close($ch);
             return json_decode($result, true);
- }
+}
   
 //include PushNotifications.php file and call sendPushNotification method on your page
  
 //require_once 'example.php';
-$device_token    =  "fOyoviWU-EQ:APA91bFYYOot6p1I8P42pi_zPQrGf8Lmzl9tYH4YEVPF-ECmmFGe_-nKMoeVa2dzX7ikAs_OBeZXK8MoY8YhpZq0AOT0Ne47NoKHRfSz_0Etk9W-7IG0qg39UppREZoIuJbod0-mTtD6";
+$to    =  "fOyoviWU-EQ:APA91bFYYOot6p1I8P42pi_zPQrGf8Lmzl9tYH4YEVPF-ECmmFGe_-nKMoeVa2dzX7ikAs_OBeZXK8MoY8YhpZq0AOT0Ne47NoKHRfSz_0Etk9W-7IG0qg39UppREZoIuJbod0-mTtD6";
 $data          =  array( 'body' => 'message text');
 
 //$response        =  PushNotifications::sendPushNotification($device_token, $fields);
