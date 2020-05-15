f1280eae464a1d1eaecb517c8d815d0e2b95ca09
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-15 23:15:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,39 +1,26 @@
 <?php
-function sendGCM($message, $id) {
 
-
-    $url = 'https://fcm.googleapis.com/fcm/send';
-
-    $fields = array (
-            'registration_ids' => array (
-                    $id
-            ),
-            'data' => array (
-                    "message" => $message
-            )
-    );
-    $fields = json_encode ( $fields );
-
-    $headers = array (
-            'Authorization: key=' . "AAAA4QcnW_8:APA91bEdc3LdsGOhcGrDuhalIi2apF-xg35gBZiqDD_L4PJ87iCLAgkioT0HKf7Yd_6BSr4YoikwgGxuWukQw8abcn9N0a1yinyshSmfpwS4G50oQQeG6eswTZ1FYq4-b71qhh7T4sPA",
-            'Content-Type: application/json'
-    );
-
-    $ch = curl_init ();
-    curl_setopt ( $ch, CURLOPT_URL, $url );
-    curl_setopt ( $ch, CURLOPT_POST, true );
-    curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
-    curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-    curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
-
-    $result = curl_exec ( $ch );
-    echo $result;
-    curl_close ( $ch );
+function sendPushNotification($to = '', $data = array()){
+	$apiKey = '';
+	$fields = array( 'to' => $to,	'notfication' => $data );
+	
+	$headers = array( 'Authorization: keys='.$apiKey, 'Content-Type: application/json');
+	
+	$url = 'https://fcm.googleapis.com/fcm/send';
+	
+	$ch = curl_init();
+	curl_setopt( $ch, CURLOPT_URL, $url);
+	curl_setopt( $ch, CURLOPT_POST, true):
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt( $ch, CURLOPT_RETURNTRANSEFER, true);
+	
+	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($fields));
+	$result = curl_exec($ch);
+	return json_decode($result, true);
 }
-
-$mess = "great scott i feel sick";
-$token = "fOyoviWU-EQ:APA91bFYYOot6p1I8P42pi_zPQrGf8Lmzl9tYH4YEVPF-ECmmFGe_-nKMoeVa2dzX7ikAs_OBeZXK8MoY8YhpZq0AOT0Ne47NoKHRfSz_0Etk9W-7IG0qg39UppREZoIuJbod0-mTtD6";
-
-sendGCM()
-
-?>
+$to ="";
+$data = array(
+	'body' => 'New message'
+);
+print_r(sendPushNotification($to, $data));	
