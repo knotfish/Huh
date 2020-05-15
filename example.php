839111e64d3723833b0b03141b32c3a9d4ce9d0d
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-15 13:31:22 +0800

    Update example.php

--- example.php
+++ example.php
@@ -1,32 +1,24 @@
 
 <?php
 
-
-//class example {
  
-private static $URL  = "https://fcm.googleapis.com/fcm/send";  //API URL of FCM
- 
-private static $API_ACCESS_KEY = 'AAAA4QcnW_8:APA91bEdc3LdsGOhcGrDuhalIi2apF-xg35gBZiqDD_L4PJ87iCLAgkioT0HKf7Yd_6BSr4YoikwgGxuWukQw8abcn9N0a1yinyshSmfpwS4G50oQQeG6eswTZ1FYq4-b71qhh7T4sPA'; // YOUR_FIREBASE_API_KEY
- 
-//public function __construct() {
- 
-
- 
-public static function sendPushNotification($token = "", $fields = array())
+public static function sendPushNotification($device_token = "", $data = array())
  {
-            $registrationIds = array();
-             
-            array_push($registrationIds, $token);
+            $API_ACCESS_KEY = 'AAAA4QcnW_8:APA91bEdc3LdsGOhcGrDuhalIi2apF-xg35gBZiqDD_L4PJ87iCLAgkioT0HKf7Yd_6BSr4YoikwgGxuWukQw8abcn9N0a1yinyshSmfpwS4G50oQQeG6eswTZ1FYq4-b71qhh7T4sPA'; // YOUR_FIREBASE_API_KEY
+			$fields  = array('to' => $device_token, 'notification' => $msg);
  
-            $msg     = array('body' => $fields['body'], 'title'  => $fields['title']);
+            $headers = array('Authorization: key='.$API_ACCESS_KEY, 'Content-Type: application/json');
  
-            $fields  = array('registration_ids' => $registrationIds, 'notification' => $msg);
+            //array_push($registrationIds, $token);
  
-            $headers = array('Authorization: key=' . self::$API_ACCESS_KEY, 'Content-Type: application/json');
- 
+            //$msg     = array('body' => $data['body'], 'title'  => $data['title']);
+			$URL  = "https://fcm.googleapis.com/fcm/send";  //API URL of FCM
+			
+            
+            
             #Send Reponse To FireBase Server    
             $ch = curl_init(); 
-            curl_setopt($ch,CURLOPT_URL, self::$URL);
+            curl_setopt($ch,CURLOPT_URL,$URL);
             curl_setopt($ch,CURLOPT_POST, true);
             curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
             curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
@@ -35,14 +27,14 @@ public static function sendPushNotification($token = "", $fields = array())
             curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($fields));
             $result = curl_exec($ch);
             curl_close($ch);
-            return $result;
+            return json_decode($result, true);
  }
   
 //include PushNotifications.php file and call sendPushNotification method on your page
  
 //require_once 'example.php';
 $device_token    =  "fOyoviWU-EQ:APA91bFYYOot6p1I8P42pi_zPQrGf8Lmzl9tYH4YEVPF-ECmmFGe_-nKMoeVa2dzX7ikAs_OBeZXK8MoY8YhpZq0AOT0Ne47NoKHRfSz_0Etk9W-7IG0qg39UppREZoIuJbod0-mTtD6";
-$fields          =  ["title" => "message title", "body" => "message text"];
+$data          =  ["title" => "message title", "body" => "message text"];
 $response        =  PushNotifications::sendPushNotification($device_token, $fields);
 print_r($response);
 
