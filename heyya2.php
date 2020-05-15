ea4566fd4937cd79ebe60094a4f29824dedc98a7
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-16 00:18:49 +0800

    s

---
+++ heyya2.php
@@ -0,0 +1,30 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n  \"data\" : {\n    \"name\" : \"Goku\",\n    \"power_level\" : \"Over 9000\",\n    \"fighting_skill\" : \"excellent\"\n  },\n  \"to\" : \"fOyoviWU-EQ:APA91bFYYOot6p1I8P42pi_zPQrGf8Lmzl9tYH4YEVPF-ECmmFGe_-nKMoeVa2dzX7ikAs_OBeZXK8MoY8YhpZq0AOT0Ne47NoKHRfSz_0Etk9W-7IG0qg39UppREZoIuJbod0-mTtD6"\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "authorization: key=AIzaSyCypTohpSRrCDlCnlA5QtYbyFlQFQj1ib8",
+    "cache-control: no-cache",
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
