c16c9961c8f088f3a8cf6529d51a94fddccd5bc8
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-22 13:06:53 +0800

    Update example.php

--- example.php
+++ example.php
@@ -4,7 +4,7 @@
 	function sendPushNotification($to = '', $data = array()){
 	
 		$apiKey = 'AAAA2eQqZ-s:APA91bE2sES__AiazY_dMmpw90hFdLXMyfhgFkuO0ExK6pYdOJUxHV4A9U7PA3DhBxswPqMnKbKeMGDkastP-YaUe-Juuxz2Wqa0fHCLRHDUcqGz9q_9zjrRfGsIB7rVK54pu5Hxx5Ff'; 
-		$fields = array('registration_ids' => $to, 'data' => $msg);
+		$fields = array('to' => $to, 'notifiction' => $msg);
 
 		$headers = array('Authorization: key='.$apiKey, 'Content-Type: application/json');
 
@@ -13,26 +13,15 @@
 		$ch = curl_init(); 
 		curl_setopt( $ch, CURLOPT_URL,$url);
 		
-
 		curl_setopt( $ch, CURLOPT_POST, true);
-
-
 
 		curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
 
-
-
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
-
-
 
 		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
 
-
-
 		curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($fields));
-
-
 
 		$result = curl_exec($ch);
 		curl_close($ch);
