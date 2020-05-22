ef0dfc0f433b1e408cded1ddd091dcfac6d7c117
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-23 03:10:23 +0800

    s

--- example.php
+++ example.php
@@ -34,4 +34,5 @@
 		'body' => 'message text'
 	);
 	print_r(sendPushNotification($to, $data));
+	
 
--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 			print("This is the index/n");
 			
 			function funk(){
-				print("This is a function/n/n/n/n");
+				print("This is a function\n");
 			}
 			
 			funk();
 			funk();
 
-			print("AVG is preventing notifications. Must find way to resolve")
+			print("AVG is preventing notifications. Must find way to resolve\n\n Can try topics kotlin or composer")
 		?>
 
