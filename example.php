2a4c39446bd96d2ce9582aae636dd5896d9cdf4a
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-29 11:08:40 +0800

    Update example.php

--- example.php
+++ example.php
@@ -9,7 +9,7 @@ $msg = $_GET['msg'];
 		
 	
 		$apiKey = 'AAAA2eQqZ-s:APA91bE2sES__AiazY_dMmpw90hFdLXMyfhgFkuO0ExK6pYdOJUxHV4A9U7PA3DhBxswPqMnKbKeMGDkastP-YaUe-Juuxz2Wqa0fHCLRHDUcqGz9q_9zjrRfGsIB7rVK54pu5Hxx5Ff'; 
-		$fields = array('registration_ids' => array($token1, $token2), 'notification' => $data);
+		$fields = array('registration_ids' => $regis, 'notification' => $data);
 
 		$headers = array('Authorization: key='.$apiKey, 'Content-Type: application/json');
 
@@ -36,6 +36,7 @@ $msg = $_GET['msg'];
 	}
 	$token1 = "e3xqFEJIREE:APA91bGkT9lPMy0hmUxm0-mE8TD19aIbQ-6NHAi2ba2R0OF3ZRP6TnLOQe3QFr9GVDIvHzPdcZJMWcB83df2eIv6PicWDyX7jf52eaGrTspiBZHVs63sz1CZMwrnsXl4jaVrqI4vMGiZ";
 	$token2 = "c903y31E3fQ:APA91bHJzXoLkizD4W3gRGbPMg3UFCEWu2FS4XdsNf7nrAOEBadk0Sbop1QnNEleAMuE4Gou7nfRpchmdbzB0z1nk2m-Z_Rbw-f6Ehr0RIiZVx6I_VEMO68iiiQgkJsbSXI_kNbOJ_in";
+	$regis = array($token1, $token2);
 	$data = array(
 		"title" => $title, 
 		"body" => $msg 
