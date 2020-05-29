0697bf92991a310d05bcab2f0f9d401e36e0eda1
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-29 13:08:24 +0800

    Update example.php

--- example.php
+++ example.php
@@ -9,11 +9,8 @@
 		
 	
 		$apiKey = 'AAAA2eQqZ-s:APA91bE2sES__AiazY_dMmpw90hFdLXMyfhgFkuO0ExK6pYdOJUxHV4A9U7PA3DhBxswPqMnKbKeMGDkastP-YaUe-Juuxz2Wqa0fHCLRHDUcqGz9q_9zjrRfGsIB7rVK54pu5Hxx5Ff'; 
-		//$fields = array('registration_ids' => $to, 'notification' => $data);
-		$fields = "to": "aUniqueKey",
-			"data": {
-				"hello": "This is a Firebase Cloud Messaging Device Group Message!",
-				}
+		$fields = array('to' => $to, 'notification' => $data);
+		
 		$headers = array('Authorization: key='.$apiKey, 'Content-Type: application/json');
 
 		$url = "https://fcm.googleapis.com/fcm/send";
@@ -29,9 +26,9 @@
 
 		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
 
-		//curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($fields));
 
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields);
+		//curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields);
 
 		$result = curl_exec($ch);
 		curl_close($ch);
