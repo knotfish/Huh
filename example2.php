4fa9ab8477b0b3501e36766c2b9b482eb28ed937
knotfish <65358096+knotfish@users.noreply.github.com>
2020-05-29 14:11:31 +0800

    s

--- example2.php
+++ example2.php
@@ -1,44 +1,51 @@
+
 <?php
 
-// API access key from Google API's Console
-define( 'API_ACCESS_KEY', 'AAAA2eQqZ-s:APA91bE2sES__AiazY_dMmpw90hFdLXMyfhgFkuO0ExK6pYdOJUxHV4A9U7PA3DhBxswPqMnKbKeMGDkastP-YaUe-Juuxz2Wqa0fHCLRHDUcqGz9q_9zjrRfGsIB7rVK54pu5Hxx5Ff' );
+$title = $_GET['title'];
+$msg = $_GET['msg'];
 
+	function sendPushNotification($to = '', $data = array()){
+	
+		
+	
+		$apiKey = 'AAAA2eQqZ-s:APA91bE2sES__AiazY_dMmpw90hFdLXMyfhgFkuO0ExK6pYdOJUxHV4A9U7PA3DhBxswPqMnKbKeMGDkastP-YaUe-Juuxz2Wqa0fHCLRHDUcqGz9q_9zjrRfGsIB7rVK54pu5Hxx5Ff'; 
+		$fields = array('to' => $to, 'notification' => $data);
+		
+		$headers = array('Authorization: key='.$apiKey, 'Content-Type: application/json');
 
-$registrationIds = array( $_GET['id'] );
-
-// prep the bundle
-$msg = array
-(
-	'message' 	=> 'here is a message. message',
-	'title'		=> 'This is a title. title',
-	'subtitle'	=> 'This is a subtitle. subtitle',
-	'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
-	'vibrate'	=> 1,
-	'sound'		=> 1,
-	'largeIcon'	=> 'large_icon',
-	'smallIcon'	=> 'small_icon'
-);
-
-$fields = array
-(
-	'registration_ids' 	=> $registrationIds,
-	'data'			=> $msg
-);
+		$url = "https://fcm.googleapis.com/fcm/send";
  
-$headers = array
-(
-	'Authorization: key=' . API_ACCESS_KEY,
-	'Content-Type: application/json'
-);
- 
-$ch = curl_init();
-curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
-curl_setopt( $ch,CURLOPT_POST, true );
-curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
-curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
-curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
-curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
-$result = curl_exec($ch );
-curl_close( $ch );
+		$ch = curl_init(); 
+		curl_setopt( $ch, CURLOPT_URL,$url);
+		
+		curl_setopt( $ch, CURLOPT_POST, true);
 
-echo $result;
+		curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers);
+
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+
+		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false);
+
+		curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($fields));
+
+		//curl_setopt( $ch, CURLOPT_POSTFIELDS, $fields);
+
+		$result = curl_exec($ch);
+		curl_close($ch);
+		return json_decode($result, true);
+	
+
+	}
+	$token1 = "e3xqFEJIREE:APA91bGkT9lPMy0hmUxm0-mE8TD19aIbQ-6NHAi2ba2R0OF3ZRP6TnLOQe3QFr9GVDIvHzPdcZJMWcB83df2eIv6PicWDyX7jf52eaGrTspiBZHVs63sz1CZMwrnsXl4jaVrqI4vMGiZ";
+	$token2 = "c903y31E3fQ:APA91bHJzXoLkizD4W3gRGbPMg3UFCEWu2FS4XdsNf7nrAOEBadk0Sbop1QnNEleAMuE4Gou7nfRpchmdbzB0z1nk2m-Z_Rbw-f6Ehr0RIiZVx6I_VEMO68iiiQgkJsbSXI_kNbOJ_in";
+	$to = "APA91bHJGdOmaj5n2hBqSHxmpdPeOJSJNDt9SrK7mEMKr-oYFYJjUvCotKn734FBivReLnj2G8-MavHtz9ZtIv2hJz_u4FkhazYJl74fomPxu5cqPgUTmTc";
+	//$regis = array($token1, $token2);
+	$data = array(
+		"title" => "letsGrooveTonight", 
+		"body" => "ifYouSaidGoodbyeToMeTonight"//$msg 
+		//"icon" => "https://example.com/icon.png",  
+		//"click_action" => "Nil"
+	);
+	print_r(sendPushNotification($to, $data));
+	//	APA91bHJGdOmaj5n2hBqSHxmpdPeOJSJNDt9SrK7mEMKr-oYFYJjUvCotKn734FBivReLnj2G8-MavHtz9ZtIv2hJz_u4FkhazYJl74fomPxu5cqPgUTmTc
+
