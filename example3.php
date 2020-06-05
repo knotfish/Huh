df987cb8af1c9222c0099d139522c3907c7e9b14
knotfish <65358096+knotfish@users.noreply.github.com>
2020-06-05 13:15:24 +0800

    Update example3.php

--- example3.php
+++ example3.php
@@ -13,7 +13,7 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
 $list = curl_exec($ch);
 curl_close($ch);
 
-$userList = json_decode($list, true);
+$userList = json_decode($list);
 
 print("1");
 	
