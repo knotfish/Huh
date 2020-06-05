fcfb9c0b04aa07c1521047a311645aa393db4afe
knotfish <65358096+knotfish@users.noreply.github.com>
2020-06-05 14:13:17 +0800

    Update jsonTest.php

--- jsonTest.php
+++ jsonTest.php
@@ -11,7 +11,7 @@ curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
 $list = curl_exec($ch);
 curl_close($ch);
 
-$userList = json_decode($list);
+$userList = json_decode($list, true);
 
 print("1");
 /*	
