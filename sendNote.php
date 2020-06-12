8bb59febad5c006a7dab703e555fba7a77ec9781
knotfish <65358096+knotfish@users.noreply.github.com>
2020-06-12 12:42:08 +0800

    Update sendNote.php

--- sendNote.php
+++ sendNote.php
@@ -7,7 +7,7 @@ $regis = array();
 
 $ch = curl_init();
 
-curl_setopt( $ch, CURLOPT_URL, 'https://nursecall2-c2dea.firebaseio.com/users.json');
+curl_setopt( $ch, CURLOPT_URL, 'https://nurseapp3.firebaseio.com/users.json');
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
 $list = curl_exec($ch);
 curl_close($ch);
